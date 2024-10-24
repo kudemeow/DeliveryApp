aa0d4020fa7488928ce71f148999de7e33a70e40
Максимова Любовь <luba.g6002@gmail.com>
2024-10-24 16:13:05 +0300

    some methods have been added

---
+++ Orders/Filtering.cs
@@ -0,0 +1,19 @@
+﻿using Orders.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    internal class Filtering
+    {
+        public static List<Order> FilterOrders(List<Order> orders, string district, DateTime firstDeliveryTime)
+        {
+            DateTime endTime = firstDeliveryTime.AddMinutes(30);
+
+            return orders.Where(x => x.District == district && x.DeliveryTime >= firstDeliveryTime && x.DeliveryTime <= endTime).ToList();
+        }
+    }
+}
--- Orders/Program.cs
+++ Orders/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
+        public void Run(string ordersFilePath, string district, DateTime firstDeliveryTime, string resultFilePath)
+        {
+            try
+            {
+                var orders = ReadingFromFile.ReadOrdersFromFile(ordersFilePath);
+                var filteredOrders = Filtering.FilterOrders(orders, district, firstDeliveryTime);
+
+                File.WriteAllLines(resultFilePath, filteredOrders.Select(o => $"{o.OrderId},{o.Weight},{o.District},{o.DeliveryTime}"));
+                Logging.Log("Filtering completed successfully");
+            }
+            catch (Exception ex)
+            {
+                Logging.Log($"Error: {ex.Message}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Usage: app.exe <ordersFilePath> <district> <firstDeliveryTime> <resultFilePath>");
+
+                return;
+            }
+
+            string ordersFilePath = args[0];
+            string district = args[1];
+
+            if (!DateTime.TryParse(args[2], out DateTime firstDeliveryTime))
+            {
+                Console.WriteLine("Invalid date format for firstDeliveryTime");
+
+                return;
+            }
+
+            string resultFilePath = args[3];
+            var app = new Orders();
+
+            app.Run(ordersFilePath, district, firstDeliveryTime, resultFilePath);
         }
     }
 }
